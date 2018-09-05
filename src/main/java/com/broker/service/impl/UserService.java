c474acd2474099a45546fe33130071c3b4773a42
jerryring <chenjiayep@gmail.com>
2018-09-05 17:44:14 +0800

    Initial commit

---
+++ src/main/java/com/broker/service/impl/UserService.java
@@ -0,0 +1,9 @@
+package com.broker.service.impl;
+
+/**
+ * @author broker
+ * @version v1.0
+ * @date 2018-09-05
+ */
+public class UserService {
+}
---
+++ src/test/java/com/broker/dao/IUserDaoTest.java
@@ -0,0 +1,9 @@
+package com.broker.dao;
+
+/**
+ * @author broker
+ * @version v1.0
+ * @date 2018-09-05
+ */
+public class IUserDaoTest {
+}
